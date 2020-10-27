5581321b9992ee01c4a9fe2f15b87d818d8eb27f
Giulia Napoli <gnapoli1996@gmail.com>
2020-10-27 15:25:49 +0100

    Corrects logistic bug

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var list = document.getElementById("result");
 
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 === 1 && i % 5 === 1) {
+  if (i % 3 === 0 && i % 5 === 0) {
     list.innerHTML += '<li>FizzBuzz</li>';
-  } else if (i % 5 === 1) {
+  } else if (i % 5 === 0) {
       list.innerHTML += "<li>Buzz</li>";
-  } else if (i % 3 === 1) {
+  } else if (i % 3 === 0) {
       list.innerHTML += "<li>Fizz</li>";
   } else {
     list.innerHTML += "<li>" + i + "</li>"
